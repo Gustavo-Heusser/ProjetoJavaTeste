7c40c668538d8c9418ead12a5fcc05961d57ca37
Gustavo-Heusser <88352197+Gustavo-Heusser@users.noreply.github.com>
2021-08-03 18:58:32 -0300

    Add files via upload

---
+++ ProjetoParaEntregar/src/projetoparaentregar/ProjetoParaEntregar.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projetoparaentregar;
+
+/**
+ *
+ * @author gustavo.heusser
+ */
+public class ProjetoParaEntregar {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    System.out.println("A = 2");
+    System.out.println("B = 7");
+    System.out.println("C = 10");
+    System.out.println("D = 4");
+    int A = 2;
+    int B = 7;
+    int C = 10;
+    int D = 4;
+    System.out.println("A + B =");
+    System.out.println(A + B);
+    System.out.println("C + D =");
+    System.out.println(C + D);
+    System.out.println("A + C =");
+    System.out.println(A + C);
+    System.out.println("B + D =");
+    System.out.println(B + D);
+       
+    }
+    
+}
